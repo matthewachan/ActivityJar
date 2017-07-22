23fe850b0cfaa87d51cf6c9f22f0f326663074d8
Matt Chan <matthew.a.chan@gmail.com>
2017-07-21 18:18:37 -0700

    Add boostrap styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,15 +9,26 @@ import AddJar from '../../containers/AddJar/AddJar';
 class App extends Component {
   render() {
     return (
-	<div>
-		<div className='App-header text-primary'>
-			<h2>Activity Jar</h2>
+	<div className='container'>
+
+		<div className='row'>
+			<div className='col App-header text-primary'>
+				<h2>Activity Jar</h2>
+			</div>
 		</div>
 
-	    <div className='container'>
-	        <AddJar/>
-	        <JarList/>
+	    <div className='row'>
+		    <div className='col'>
+		        <AddJar/>
+	        </div>
 	    </div>
+
+	    <div className='row'>
+	    	<div className='col'>
+		        <JarList/>
+	        </div>
+	    </div>
+	    
 	</div>
     );
   }
