03bf15cf44ac2f8cc84ea41e56965bd9d73e5340
Matthew Chan <matthew.a.chan@gmail.com>
2017-07-26 01:23:10 +0000

    Create GooglePlacesResults component

--- src/components/ActivityPage/ActivityPage.js
+++ src/components/ActivityPage/ActivityPage.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import AddActivity from '../../containers/AddActivity/AddActivity';
 import ActivityList from '../../containers/ActivityList/ActivityList';
+import GooglePlacesResults from '../../containers/GooglePlacesResults/GooglePlacesResults';
 
 class ActivityPage extends Component {
 	render() {
+		const { store } = this.context;
+		this.jar = store.getState()[this.props.match.params.jar_id];
 		return (
-			<div> 
-				<AddActivity {...this.props} />
-				<ActivityList {...this.props} />
+			<div className='container'>
+				<div className='row'>		
+					<div className='col text-primary App-header'>
+						<h2>{this.jar.label}</h2>
+					</div>
+				</div>
+				<div className='row'>		
+					<div className='col'>
+						<AddActivity {...this.props} />
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col'>
+						<ActivityList {...this.props} />
+					</div>
+				</div>
+				<div className='row'>
+					<div className='col'>
+						<GooglePlacesResults {...this.props} />
+					</div>
+				</div>
 			</div>
 		);
 	}
 }
+ActivityPage.contextTypes = {
+  store: PropTypes.object
+};
 
 export default ActivityPage;
--- src/containers/GooglePlacesResults/GooglePlacesResults.js
+++ src/containers/GooglePlacesResults/GooglePlacesResults.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import Async from 'react-promise';
 
-class DrawButton extends Component {
+import Async from 'react-promise';
+import PropTypes from 'prop-types';
+
+
+
+class GooglePlacesResults extends Component {
     render() {
-        return <Async before={(handleProm) => <button onClick={() => handleProm(this.props.promise())}>Click me!</button>} then={(json) => <div>{json.results.map(result => <li>{result.name}</li>)}</div>} />
+		const { store } = this.context;
+		let promise = () => {
+			const activities = store.getState()[this.props.match.params.jar_id].activities;
+			let r = Math.floor(Math.random() * activities.length);
+			let randActivity = activities[r].activity.toLowerCase();
+			let url ='https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.774929,-122.419416&radius=1500&key=AIzaSyBXMLM_kH6IVG8NsBU6KcTvbpHP2oZUgtM&keyword=' + randActivity
+			return fetch(url)
+    		.then(res => res.json())
+		};
+    
+        return (
+            <Async before={(handleProm) => <button className='btn btn-primary'onClick={() => handleProm(promise())}>Click me!</button>} then={(json) => <div>{json.results.map(result => <li>{result.name}</li>)}</div>} />
+        );
     }
 }
 
-const mapStateToProps = (state, ownProps) => {
-    const activities = state[ownProps.match.params.jar_id].activities;
-    const randomActivity = activities[Math.floor(Math.random() * activities.length)];
-    let url ='https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=37.774929,-122.419416&radius=1500&key=AIzaSyBXMLM_kH6IVG8NsBU6KcTvbpHP2oZUgtM&keyword=' + randomActivity;
-    const GooglePromise = () => {
-        fetch(url).then(res => res.json())
-    };
-    return {
-      promise: GooglePromise
-    };
+GooglePlacesResults.contextTypes = {
+  store: PropTypes.object
 };
 
-const GooglePlacesResults = connect(mapStateToProps, null)(DrawButton);
 export default GooglePlacesResults;
