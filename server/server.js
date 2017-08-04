4fdb56a8734536ac80c39b1936313dea5b0ea54e
Matt Chan <matthew.a.chan@gmail.com>
2017-08-03 22:07:40 -0700

    Connect frontend and backend

--- server/server.js
+++ server/server.js
@@ -19,13 +19,21 @@ app.use(cors()); // Add CORs headers
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms')); // Logging
 
 const checkAuth = jwt({
-  secret: 'TAbjlWU6QEjfu6KcAAPM4TDyUsdgrCVhcINjuZEwgBsfkcYz0klQ--08g8Y4q-lT',
-  audience: 'IoTrTeaOoYnP1SywZ5N4lgofF8f7sMmd'
+    secret: 'TAbjlWU6QEjfu6KcAAPM4TDyUsdgrCVhcINjuZEwgBsfkcYz0klQ--08g8Y4q-lT',
+    audience: 'IoTrTeaOoYnP1SywZ5N4lgofF8f7sMmd'
 });
 
-mongoose.connect('mongodb://dev_user:Graey421@ds147872.mlab.com:47872/activityjar').then(
-	function success() { console.log('Connected to mLabs database.') }, 
-	function error() { console.log('Failed to connect to mLabs database.') }
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://dev_user:Graey421@ds147872.mlab.com:47872/activityjar', {
+    useMongoClient: true
+}).then(
+    function success() {
+        console.log('Connected to mLabs database.')
+    },
+    function error() {
+        console.log('Failed to connect to mLabs database.')
+    }
 );
 
 
@@ -33,69 +41,82 @@ mongoose.connect('mongodb://dev_user:Graey421@ds147872.mlab.com:47872/activityja
 
 // Register new user (POST)
 app.get('/users', (req, res) => {
-	user_collection.findUsers().then(users => {
-		res.json(users);
-	});
-	
-});
+    user_collection.findUsers().then(users => {
+        res.json(users);
+    });
 
-app.post('/user', (req, res) => {
-	var newUser;
-	if (req.query.user_id) {
-		newUser = user_collection.saveUser(req.query.user_id);
-	}
-	res.json(newUser);
 });
-
 // Get all jars that are either owned, editable, or viewable by a user (GET)
 // Look at docs here: https://docs.mongodb.com/manual/tutorial/query-arrays/ 
 app.get('/jars', (req, res) => {
-	// var user_id = req.query.user_id;
-	// var query = { $or: [{ viewable_by: user_id }, { editable_by: user_id }, { owner: user_id }] };
-	Jar.find({}, (err, jars) => {
-		if (err)
-			console.log(err);
-		res.json(jars);
-	})	
+    var user_id = req.query.user_id;
+    var query = { $or: [{ viewable_by: user_id }, { editable_by: user_id }, { owner_id: user_id }] };
+    Jar.find(query, (err, jars) => {
+        if (err)
+            console.log(err);
+        res.json(jars);
+    })
 });
+
+
+
+app.post('/user', (req, res) => {
+    var newUser;
+    if (req.query.user_id) {
+        newUser = user_collection.saveUser(req.query.user_id);
+    }
+    res.json(newUser);
+});
+
 
 // Add a new jar
 app.post('/jar', (req, res) => {
-	var newJar;
-	if (req.query.label) {
-		newJar = new Jar({ jar_id: hash(req.query.label), label: req.query.label, activities: {}, viewable_by: [], editable_by: [] });
-		newJar.save( (err, success) => {
-			if (err)
-				console.log('Failed to add new jar.');
-			else
-				console.log('Added a new jar successfully.');
-		})
-	}
-	res.json(newJar);
+    var newJar;
+    if (req.query.label) {
+        newJar = new Jar({
+            jar_id: hash(req.query.label),
+            label: req.query.label,
+            activities: {},
+            viewable_by: [],
+            editable_by: []
+        });
+        newJar.save((err, success) => {
+            if (err)
+                console.log('Failed to add new jar.');
+            else
+                console.log('Added a new jar successfully.');
+        })
+    }
+    res.json(newJar);
 });
 
 // Add a new activity
 app.post('/jar/:jar_id', (req, res) => {
-	if (req.params.jar_id)
-		Jar.findOne({ jar_id : parseInt(req.params.jar_id, 10) }, (err, jar) => {
-			if (err)
-				console.log('Could not find the jar to update.');
-			else {
-				var newActivity = {};
-				var activity_id = hash(req.query.activity).toString();
-				newActivity[activity_id] = {repeat: false, activity: req.query.activity};
-				jar.activities = Object.assign({}, jar.activities, newActivity);
-				jar.save( (err, updatedJar) => {
-					if (err)
-						console.log('Failed to update jar.');
-					res.json(jar);
-				})
-				
-			}
-		});
+    if (req.params.jar_id)
+        Jar.findOne({
+            jar_id: parseInt(req.params.jar_id, 10)
+        }, (err, jar) => {
+            if (err)
+                console.log('Could not find the jar to update.');
+            else {
+                var newActivity = {};
+                var activity_id = hash(req.query.activity).toString();
+                newActivity[activity_id] = {
+                    repeat: false,
+                    activity: req.query.activity
+                };
+                jar.activities = Object.assign({}, jar.activities, newActivity);
+                jar.save((err, updatedJar) => {
+                    if (err)
+                        console.log('Failed to update jar.');
+                    res.json(jar);
+                })
+
+            }
+        });
 });
 
-app.listen(PORT, '0.0.0.0');
+app.listen(PORT);
 console.log('ActivityJar REST API listening on port ' + PORT);
 
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -9,53 +9,77 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const preloadedState = {
-  2553487358: {
-    label: 'Food Jar',
-    activities: {
-      179021649: {
-        activity: 'Sushi',
-        repeat: false
-      },
-      2088074780: {
-        activity: 'Pasta',
-        repeat: false
-      },
-      170218834: {
-        activity: 'Soup',
-        repeat: false
-      }
-    }
-  },
-  1472647057: {
-    jar_id: 1,
-    label: 'Activity Jar',
-    activities: {
-      2888167059: {
-        activity: 'Bowling',
-        repeat: true
-      },
-      395274124: {
-        activity: 'Basketball',
-        repeat: false
-      },
-      760014489: {
-        activity: 'Rock Climbing',
-        repeat: false
-      }
-    }
-  }
-};
+function getStateFromCollection(collection) {
+  var state = {};
 
-const store = createStore(reducer, preloadedState);
+  collection.forEach(doc => {
+    state[doc.jar_id] = {
+      label: doc.label,
+      owner_id: doc.owner_id,
+      viewable_by: doc.viewable_by,
+      editable_by: doc.editable_by,
+      activities: doc.activities
+    };
+  })
+
+  return state;
+}
+
+global.fetch('http://localhost:8080/jars?user_id=54321')
+.then(res => res.json()
+  .then(data => getStateFromCollection(data))
+  .then(state => {
+    const store = createStore(reducer, state);
+    ReactDOM.render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>, document.getElementById('root')
+    );
+    registerServiceWorker();
+  })
+)
 
 
-ReactDOM.render(
-	<Provider store={store}>
-    <BrowserRouter>
-  		<App />
-    </BrowserRouter>
-	</Provider>, document.getElementById('root')
-);
 
-registerServiceWorker();
+// })
+
+// const preloadedState = {
+//   2553487358: {
+//     label: 'Food Jar',
+//     activities: {
+//       179021649: {
+//         activity: 'Sushi',
+//         repeat: false
+//       },
+//       2088074780: {
+//         activity: 'Pasta',
+//         repeat: false
+//       },
+//       170218834: {
+//         activity: 'Soup',
+//         repeat: false
+//       }
+//     }
+//   },
+//   1472647057: {
+//     jar_id: 1,
+//     label: 'Activity Jar',
+//     activities: {
+//       2888167059: {
+//         activity: 'Bowling',
+//         repeat: true
+//       },
+//       395274124: {
+//         activity: 'Basketball',
+//         repeat: false
+//       },
+//       760014489: {
+//         activity: 'Rock Climbing',
+//         repeat: false
+//       }
+//     }
+//   }
+// };
+
